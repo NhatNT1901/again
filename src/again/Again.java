f7437b33379e4204cef7b092d3e3a2fc10e172f2
NhatNT1901 <tinywormtx@gmail.com>
2023-05-09 22:04:54 +0700

    again

---
+++ src/again/Again.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package again;
+
+/**
+ *
+ * @author Admin
+ */
+public class Again {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi there");
+    }
+    
+}
